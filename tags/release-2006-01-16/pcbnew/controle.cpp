8c6a43e351e128192a4f025f7274c7ca9ffbaad2
bokeoa <bokeoa@244deca0-f506-0410-ab94-f4f3571dea26>
2006-02-17 19:31:57 +0000

    Tagging the 2006-01-16 release

---
+++ tags/release-2006-01-16/pcbnew/controle.cpp
@@ -0,0 +1,288 @@
+		/********************************************************/
+		/* Routines generales de gestion des commandes usuelles */
+		/********************************************************/
+
+	/* fichier controle.cpp */
+/*
+ Routines d'affichage grille, Boite de coordonnees, Curseurs, marqueurs ...
+*/
+
+#include "fctsys.h"
+#include "gr_basic.h"
+
+#include "common.h"
+#include "pcbnew.h"
+
+#include "id.h"
+#include "protos.h"
+
+/* Routines Locales : */
+
+/* Variables Locales */
+
+/**********************************/
+void RemoteCommand(char * cmdline)
+/**********************************/
+{
+char Line[1024];
+wxString msg;
+char *idcmd, * text;
+WinEDA_PcbFrame * frame = EDA_Appl->m_PcbFrame;
+
+	strncpy(Line, cmdline, sizeof(Line) -1 );
+	frame->Affiche_Message( CONV_FROM_UTF8(Line));
+
+	idcmd = strtok(Line," \n\r");
+	text = strtok(NULL," \n\r");
+	if( strcmp(idcmd,"$PART:") == 0)
+	{
+		MODULE * Module;
+		msg = CONV_FROM_UTF8(text);
+		Module = ReturnModule(frame->m_Pcb, msg);
+		msg.Printf(_("Locate module %s %s"),msg.GetData(), Module ? wxT("Ok") : wxT("not found"));
+		frame->SetStatusText(msg);
+		if ( Module )
+		{
+	wxClientDC dc(frame->DrawPanel);
+			frame->DrawPanel->PrepareGraphicContext(&dc);
+			frame->GetScreen()->Trace_Curseur(frame->DrawPanel, &dc);
+			frame->GetScreen()->m_Curseur = Module->m_Pos;
+			frame->GetScreen()->Trace_Curseur(frame->DrawPanel, &dc);
+		}
+	}
+
+	if( strcmp(idcmd,"$PIN:") == 0)
+	{
+		wxString PinName, ModName;
+		MODULE * Module;
+		D_PAD * Pad = NULL;
+		int netcode = -1;
+		PinName = CONV_FROM_UTF8(text);
+		text = strtok(NULL," \n\r");
+		if( strcmp(text, "$PART:") == 0 ) text = strtok(NULL,"\n\r");
+
+wxClientDC dc(frame->DrawPanel);
+	frame->DrawPanel->PrepareGraphicContext(&dc);
+
+		ModName = CONV_FROM_UTF8(text);
+		Module = ReturnModule(frame->m_Pcb, ModName);
+		if( Module ) Pad = ReturnPad(Module, PinName);
+		if( Pad ) netcode = Pad->m_NetCode;
+		if ( netcode > 0 )
+			{
+			/* effacement surbrillance ancienne */
+			if(g_HightLigt_Status) frame->Hight_Light(&dc);
+			g_HightLigth_NetCode = netcode;
+			frame->Hight_Light(&dc);
+			frame->GetScreen()->Trace_Curseur(frame->DrawPanel, &dc);
+			frame->GetScreen()->m_Curseur = Pad->m_Pos;
+			frame->GetScreen()->Trace_Curseur(frame->DrawPanel, &dc);
+			}
+
+		if ( Module == NULL )
+			msg.Printf( _("module %s not found"), text);
+		else if ( Pad == NULL )
+			msg.Printf( _("Pin %s (module %s) not found"), PinName.GetData(), ModName.GetData());
+		else
+			msg.Printf( _("Locate Pin %s (module %s)"), PinName.GetData(),ModName.GetData());
+		frame->Affiche_Message(msg);
+	}
+}
+
+
+/***********************************************************************/
+EDA_BaseStruct * WinEDA_BasePcbFrame::PcbGeneralLocateAndDisplay(void)
+/***********************************************************************/
+{
+	return Locate(CURSEUR_OFF_GRILLE);
+}
+
+
+/****************************************************************/
+void WinEDA_BasePcbFrame::GeneralControle(wxDC *DC, wxPoint Mouse)
+/*****************************************************************/
+{
+int ll;
+wxSize delta;
+int zoom = GetScreen()->GetZoom();
+wxPoint curpos, oldpos;
+int hotkey = 0;
+
+	ActiveScreen = GetScreen();
+
+	// Sauvegarde du board si necessaire:
+int CurrentTime = time(NULL);
+	if( !GetScreen()->IsModify() || GetScreen()->IsSave() )
+	{		/* Sauvegarde inutile */
+		g_SaveTime = CurrentTime;
+	}
+
+	if ( (CurrentTime - g_SaveTime) > g_TimeOut)
+	{
+		wxString tmpFileName = GetScreen()->m_FileName;
+		wxString filename = g_SaveFileName + PcbExtBuffer;
+		((WinEDA_PcbFrame*)this)->SavePcbFile(filename);
+		GetScreen()->m_FileName = tmpFileName;
+		SetTitle(GetScreen()->m_FileName);
+	}
+
+	curpos = DrawPanel->CursorRealPosition(Mouse);
+	oldpos = GetScreen()->m_Curseur;
+
+	delta.x = (int) round((double)GetScreen()->GetGrid().x / zoom);
+	delta.y = (int) round((double)GetScreen()->GetGrid().y / zoom);
+	if( delta.x <= 0 ) delta.x = 1;
+	if( delta.y <= 0 ) delta.y = 1;
+
+	switch(g_KeyPressed)
+		{
+		case WXK_NUMPAD_SUBTRACT :
+		case WXK_SUBTRACT :
+		case '-' :
+			ll = GetScreen()->m_Active_Layer;
+			if(ll > CMP_N) break;
+			if(ll <= CUIVRE_N) break;
+			if ( m_Pcb->m_BoardSettings->m_CopperLayerCount <= 1)	// Single layer
+				ll = CUIVRE_N;
+			if ( ll == CMP_N )
+				ll = MAX(CUIVRE_N, m_Pcb->m_BoardSettings->m_CopperLayerCount-2);
+			else if ( ll > CUIVRE_N) ll--;
+			GetScreen()->m_Active_Layer = ll;
+			if ( DisplayOpt.ContrastModeDisplay ) DrawPanel->Refresh(TRUE);
+			break ;
+
+		case WXK_NUMPAD_ADD :
+		case WXK_ADD :
+		case '+' :
+			ll = GetScreen()->m_Active_Layer;
+			if(ll >= CMP_N) break;
+			ll++;
+			if ( ll >= m_Pcb->m_BoardSettings->m_CopperLayerCount-1 )
+				ll = CMP_N;
+			if ( m_Pcb->m_BoardSettings->m_CopperLayerCount <= 1)	// Single layer
+				ll = CUIVRE_N;
+			GetScreen()->m_Active_Layer = ll;
+			if ( DisplayOpt.ContrastModeDisplay ) DrawPanel->Refresh(TRUE);
+			break ;
+
+		case WXK_NUMPAD0 :
+		case WXK_PRIOR :
+			if ( GetScreen()->m_Active_Layer != CMP_N )
+			{
+				GetScreen()->m_Active_Layer = CMP_N;
+				if ( DisplayOpt.ContrastModeDisplay )
+					GetScreen()->SetRefreshReq();
+			}
+			break ;
+
+		case WXK_NUMPAD9 :
+		case WXK_NEXT :
+			if ( GetScreen()->m_Active_Layer != CUIVRE_N )
+			{
+				GetScreen()->m_Active_Layer = CUIVRE_N;
+				if ( DisplayOpt.ContrastModeDisplay )
+					GetScreen()->SetRefreshReq();
+			}
+			break ;
+
+		case 'F' | GR_KB_CTRL :
+		case 'f' | GR_KB_CTRL:
+			DisplayOpt.DisplayPcbTrackFill ^= 1; DisplayOpt.DisplayPcbTrackFill &= 1 ;
+			GetScreen()->SetRefreshReq();
+			break ;
+
+		case ' ' : /* Mise a jour de l'origine des coord relatives */
+			GetScreen()->m_O_Curseur = GetScreen()->m_Curseur;
+			break ;
+
+
+		case 'U' | GR_KB_CTRL :
+		case 'u' | GR_KB_CTRL :
+			UnitMetric = (UnitMetric == INCHES ) ? MILLIMETRE : INCHES;
+			break ;
+
+		case WXK_F1 :
+			OnZoom(ID_ZOOM_PLUS_KEY);
+			curpos = GetScreen()->m_Curseur;
+			break;
+
+		case WXK_F2 :
+			OnZoom(ID_ZOOM_MOINS_KEY);
+			curpos = GetScreen()->m_Curseur;
+			break;
+
+		case WXK_F3 :
+			OnZoom(ID_ZOOM_REDRAW_KEY);
+			break;
+
+		case WXK_F4 :
+			OnZoom(ID_ZOOM_CENTER_KEY);
+			curpos = GetScreen()->m_Curseur;
+			break;
+
+		case WXK_F5 :	/* unused */
+			break;
+
+		case WXK_NUMPAD8  :	/* Deplacement curseur vers le haut */
+		case WXK_UP	:
+			Mouse.y -= delta.y;
+			DrawPanel->MouseTo(Mouse);
+			break ;
+
+		case WXK_NUMPAD2:	/* Deplacement curseur vers le bas */
+		case WXK_DOWN:
+			Mouse.y += delta.y;
+			DrawPanel->MouseTo(Mouse);
+			break ;
+
+		case WXK_NUMPAD4:	/* Deplacement curseur vers la gauche */
+		case WXK_LEFT :
+			Mouse.x -= delta.x;
+			DrawPanel->MouseTo(Mouse);
+			break ;
+
+		case WXK_NUMPAD6:  /* Deplacement curseur vers la droite */
+		case WXK_RIGHT:
+			Mouse.x += delta.x;
+			DrawPanel->MouseTo(Mouse);
+			break ;
+
+		default: hotkey = g_KeyPressed;
+			break;
+		}
+	/* Recalcul de la position du curseur schema */
+	GetScreen()->m_Curseur = curpos;
+	/* Placement sur la grille generale */
+	PutOnGrid( & GetScreen()->m_Curseur);
+
+	if( GetScreen()->IsRefreshReq() )
+		{
+		RedrawActiveWindow(DC, TRUE);
+		}
+
+	if ( (oldpos.x != GetScreen()->m_Curseur.x) ||
+		 (oldpos.y != GetScreen()->m_Curseur.y) )
+		{
+		curpos = GetScreen()->m_Curseur;
+		GetScreen()->m_Curseur = oldpos;
+		GetScreen()->Trace_Curseur(DrawPanel, DC);
+
+		GetScreen()->m_Curseur = curpos;
+		GetScreen()->Trace_Curseur(DrawPanel, DC);
+
+		if(GetScreen()->ManageCurseur)
+			{
+			GetScreen()->ManageCurseur(DrawPanel, DC, TRUE);
+			}
+		}
+
+	SetToolbars();
+	Affiche_Status_Box();	 /* Affichage des coord curseur */
+
+	if ( hotkey )
+	{
+		OnHotKey(DC, hotkey, NULL);
+	}
+}
+
+
---
+++ tags/release-2006-01-16/pcbnew/deltrack.cpp
@@ -0,0 +1,175 @@
+		/*********************************************/
+		/* Edition des pistes: Routines d'effacement */
+		/* Effacement de segment, piste, net et zone */
+		/*********************************************/
+
+#include "fctsys.h"
+#include "gr_basic.h"
+
+#include "common.h"
+#include "pcbnew.h"
+
+#include "protos.h"
+
+/* Routines Locales */
+
+/* Variables locales */
+
+
+/***************************************************************/
+TRACK * WinEDA_PcbFrame::Delete_Segment(wxDC * DC, TRACK *Track)
+/***************************************************************/
+/* Supprime 1 segment de piste.
+	2 Cas possibles:
+	Si On est en trace de nouvelle piste: Effacement du segment en
+		cours de trace
+	Sinon : Effacment du segment sous le curseur.
+*/
+{
+int current_net_code;
+
+	if ( Track == NULL ) return NULL;
+
+	if(Track->m_Flags & IS_NEW)  // Trace en cours, on peut effacer le dernier segment
+		{
+		if(nbptnewpiste > 0 )
+			{
+			int previous_layer = GetScreen()->m_Active_Layer;
+			// effacement de la piste en cours
+			Montre_Position_New_Piste(DrawPanel, DC, FALSE);
+
+			// modification du trace
+			Track = ptnewpiste; ptnewpiste = (TRACK*) ptnewpiste->Pback;
+			delete Track;
+			nbptnewpiste--;
+
+			if( nbptnewpiste && (ptnewpiste->m_StructType == TYPEVIA))
+				{
+				Track = ptnewpiste;
+				ptnewpiste = (TRACK*) ptnewpiste->Pback;
+				delete Track;
+				nbptnewpiste--;
+				if (ptnewpiste && (ptnewpiste->m_StructType != TYPEVIA) )
+                	previous_layer = ptnewpiste->m_Layer;
+				}
+			if( ptnewpiste ) ptnewpiste->Pnext = NULL;
+
+			// Rectification couche active qui a pu changer si une via
+			// a ete effacee
+			GetScreen()->m_Active_Layer = previous_layer;
+
+			Affiche_Status_Box();
+
+			if(nbptnewpiste == 0 )
+				{
+				GetScreen()->ManageCurseur = NULL;
+				GetScreen()->ForceCloseManageCurseur = NULL;
+				if(g_HightLigt_Status) Hight_Light(DC);
+				return NULL;
+				}
+			else
+				{
+				if(GetScreen()->ManageCurseur)
+					GetScreen()->ManageCurseur(DrawPanel, DC, FALSE);
+				return ptnewpiste;
+				}
+			}
+		return NULL;
+		} // Fin traitement si trace en cours
+
+
+	current_net_code = Track->m_NetCode;
+	Track->Draw(DrawPanel, DC, GR_XOR) ;
+
+	SaveItemEfface( Track, 1);
+	GetScreen()->SetModify();
+
+	test_1_net_connexion(DC, current_net_code);
+	Affiche_Infos_Status_Pcb(this);
+	return NULL;
+}
+
+
+
+/**********************************************************/
+void WinEDA_PcbFrame::Delete_Track(wxDC * DC, TRACK *Track)
+/**********************************************************/
+{
+	if( Track != NULL )
+		{
+		int current_net_code = Track->m_NetCode;
+		Supprime_Une_Piste(DC, Track) ;
+		GetScreen()->SetModify();
+		test_1_net_connexion(DC, current_net_code);
+		Affiche_Infos_Status_Pcb(this);
+		}
+}
+
+
+	/*************************/
+	/* void Delete_net(void) */
+	/*************************/
+
+void WinEDA_PcbFrame::Delete_net(wxDC * DC, TRACK *Track)
+{
+TRACK *pt_segm, * pt_start;
+int ii ;
+int net_code_delete;
+
+	pt_segm = Track;
+
+	if ( pt_segm == NULL ) return;
+
+	if ( IsOK(this, _("Delete NET ?") ) )
+		{
+		net_code_delete = pt_segm->m_NetCode;
+		/* Recherche du debut de la zone des pistes du net_code courant */
+		pt_start = m_Pcb->m_Track->GetStartNetCode(net_code_delete);
+
+		/* Decompte du nombre de segments de la sous-chaine */
+		pt_segm = pt_start;
+		for ( ii = 0 ; pt_segm != NULL; pt_segm = (TRACK*)pt_segm->Pnext )
+			{
+			if( pt_segm->m_NetCode != net_code_delete ) break;
+			ii++;
+			}
+
+		Trace_Une_Piste(DrawPanel, DC, pt_start,ii,GR_XOR);
+
+		SaveItemEfface( pt_start, ii);
+		GetScreen()->SetModify();
+		test_1_net_connexion(DC, net_code_delete);
+		Affiche_Infos_Status_Pcb(this);
+		}
+}
+
+
+/********************************************************************/
+void WinEDA_PcbFrame::Supprime_Une_Piste(wxDC * DC, TRACK* pt_segm)
+/********************************************************************/
+/* Routine de suppression de 1 piste:
+	le segment pointe est supprime puis les segments adjacents
+	jusqu'a un pad ou un point de jonction de plus de 2 segments
+*/
+{
+TRACK * pt_track, * Struct;
+int ii, nb_segm;
+
+	if (pt_segm == NULL ) return ;
+
+	pt_track = Marque_Une_Piste(this, DC, pt_segm,
+					 & nb_segm, GR_OR | GR_SURBRILL);
+
+	if(nb_segm) /* Il y a nb_segm segments de piste a effacer */
+		{
+		Trace_Une_Piste(DrawPanel, DC, pt_track,nb_segm,GR_XOR | GR_SURBRILL);
+		/* Effacement flag BUSY */
+		Struct = pt_track; ii = 0;
+		for ( ; ii < nb_segm; ii++, Struct = (TRACK*) Struct->Pnext )
+			{
+			Struct->SetState(BUSY,OFF);
+			}
+		SaveItemEfface( pt_track,  nb_segm);
+		}
+}
+
